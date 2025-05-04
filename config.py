9dbf916c1ae485317754bd811d42c8a28f60aba7
Ruihan Zhao <ruihan.zhao@se21.qmul.ac.uk>
2025-05-04 18:49:49 +0100

    debug train

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ def arg_test():
     parser.add_argument('--max_samples', type=int, default=None)
 
     # 训练参数
-    parser.add_argument('--epochs', type=int, default=200)
-    parser.add_argument('--batch_size', type=int, default=48)
-    parser.add_argument('--lr', type=float, default=3e-4)
-    parser.add_argument('--min_delta', type=float, default=1e-4)
-    parser.add_argument('--patience', type=int, default=20)
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--min_delta', type=float, default=1e-5)
+    parser.add_argument('--patience', type=int, default=2)
     parser.add_argument('--use_fp16', action='store_true', default=True)
-    parser.add_argument('--warmup_epochs', type=int, default=5)
-    parser.add_argument('--val_viz_num', type=int, default=4)
-    parser.add_argument('--num_workers', type=int, default=24)
+    parser.add_argument('--warmup_epochs', type=int, default=2)
+    parser.add_argument('--val_viz_num', type=int, default=2)
+    parser.add_argument('--num_workers', type=int, default=4)
 
     # 分布式参数
     parser.add_argument('--dist', action='store_true', default=True)
